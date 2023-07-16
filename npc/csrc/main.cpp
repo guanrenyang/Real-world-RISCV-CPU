e621ac5fb8dced5cbd3acb807e427c0b8900f3eb
tracer-ysyx <tracer@ysyx.org>
2023-07-16 11:33:59 +0800

    > sim RTL ysyx_22040000 RenyangGuan Linux ubuntu-linux-22-04-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  11:33:59 up 26 min,  1 user,  load average: 0.80, 0.42, 0.26

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,4 +6,25 @@
 // }
 
 
+#include <nvboard.h>
+#include <Vtop.h>
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle() {
+  dut.a = rand() & 1;
+  dut.b = rand() & 1;
+}
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
+}
 
