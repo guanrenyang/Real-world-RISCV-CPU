5dd4338fb21a5f580152da8953fdf60acc975314
tracer-ysyx <tracer@ysyx.org>
2023-07-16 16:29:03 +0800

    > sim RTL ysyx_22040000 RenyangGuan Linux ubuntu-linux-22-04-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  16:29:03 up 53 min,  1 user,  load average: 0.55, 0.43, 0.29

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,11 +1,4 @@
-// #include <stdio.h>
-
-// int main() {
-//   printf("Hello, ysyx!\n");
-//   return 0;
-// }
-
-
+/*LED*/
 #include <nvboard.h>
 #include <Vtop.h>
 
@@ -14,17 +7,58 @@ static TOP_NAME dut;
 void nvboard_bind_all_pins(Vtop* top);
 
 static void single_cycle() {
-  dut.a = rand() & 1;
-  dut.b = rand() & 1;
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0)
+    single_cycle();
+  dut.rst = 0;
 }
 
 int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
 
-  while(1) {
+  reset(10);
+
+  while (1)
+  {
     nvboard_update();
     single_cycle();
   }
+  
 }
+/* example*/
+// #include <stdio.h>
+
+// int main() {
+//   printf("Hello, ysyx!\n");
+//   return 0;
+// }
+
+/* On-off Switch*/
+// #include <nvboard.h>
+// #include <Vtop.h>
+
+// static TOP_NAME dut;
+
+// void nvboard_bind_all_pins(Vtop* top);
+
+// static void single_cycle() {
+//   dut.eval();
+//   printf("a: %d, b: %d, f: %d", dut.a, dut.b, dut.f);
+// }
+
+// int main() {
+//   nvboard_bind_all_pins(&dut);
+//   nvboard_init();
+
+//   while(1) {
+//     nvboard_update();
+//     single_cycle();
+//   }
+// }
 
