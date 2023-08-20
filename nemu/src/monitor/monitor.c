8a7aa82eed81df011e8c73047843ab04c5a38849
tracer-ysyx <tracer@ysyx.org>
2023-08-20 16:16:14 +0800

    > compile NEMU ysyx_22040000 RenyangGuan Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  16:16:14 up 18 min,  2 users,  load average: 0.14, 0.14, 0.13

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -32,8 +32,6 @@ static void welcome() {
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
-  Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(0);
 }
 
 #ifndef CONFIG_TARGET_AM
@@ -57,7 +55,7 @@ static long load_img() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  
+
   Log("The image is %s, size = %ld", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
