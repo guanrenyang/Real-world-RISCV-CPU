9215689b4ee6ebb14307ffed104f7da55d76d0fd
tracer-ysyx <tracer@ysyx.org>
2023-08-21 17:57:45 +0800

    > compile NEMU ysyx_22040000 RenyangGuan Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  17:57:45 up  3:10,  3 users,  load average: 0.58, 0.56, 0.32

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,7 +51,9 @@ void init_mem() {
   int i;
   for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
     p[i] = rand();
-    printf("p[%d]: %u\n", i, p[i]);
+    printf("size of p[i]: %lu\n", sizeof(p[i]));
+    if (i<10)
+      printf("p[%d]: %u\n", i, p[i]);
   }
 #endif
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
