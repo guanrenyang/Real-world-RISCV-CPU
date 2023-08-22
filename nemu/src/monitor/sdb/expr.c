b8227e395fb9fa04f8124932380e347cb18e2c08
tracer-ysyx <tracer@ysyx.org>
2023-08-22 19:03:08 +0800

    > compile NEMU ysyx_22040000 RenyangGuan Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  19:03:08 up  3:53,  2 users,  load average: 0.14, 0.18, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -37,7 +37,7 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
-  {"(?<=\\D)[1-9]\\d*(?=\\D)", TK_POS_INT}, // positive integer
+  {"(\\D|^)[1-9]\\d*(\\D|$)", TK_POS_INT}, // positive integer
   // {"[1-9]\\d*\\b", TK_POS_INT}, // positive integer
   // {"[1-9]\\d*", TK_POS_INT}, // positive integer
   {"\\+", '+'},         // plus
