5e1d33021c20b76ea9555b730bad6214891890ca
tracer-ysyx <tracer@ysyx.org>
2023-08-25 22:42:06 +0800

    > compile NEMU ysyx_22040000 RenyangGuan Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  22:42:06 up 40 min,  3 users,  load average: 0.20, 0.21, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -120,7 +120,13 @@ static bool make_token(char *e) {
                 break;
             default: TODO();
         }
-
+        
+        // For testing
+        char *test_substr = malloc(sizeof(char)*substr_len+1);
+        memset(test_substr, '\0', substr_len+1);
+        strncpy(test_substr, substr_start, substr_len);
+        printf("%s\n", test_substr); 
+        free(test_substr);
         break;
       }
     }
