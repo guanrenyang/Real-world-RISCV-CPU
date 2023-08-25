2a979be747c98f0b244994ec50faa100bede9b40
Guan Renyang <renyangguan@outlook.com>
2023-08-25 11:38:54 +0800

    Finish a plain version of `gen_expr`. There are two problems: 1. Integer overflow 2. Division by zero

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -33,9 +33,9 @@ static char *code_format =
 
 static int buf_idx = 0;
 
-// inline uint32_t choose(uint32_t n){
-//   return rand() % n;
-// }
+static inline uint32_t choose(uint32_t n){
+  return rand() % n;
+}
 static void gen_num(){
   uint32_t num = rand()%200; // Every operand should be smaller than 200
 
@@ -43,52 +43,61 @@ static void gen_num(){
   int substr_len = strlen(buf + buf_idx);
   buf_idx += substr_len; 
 }
-// static void gen_rand_expr() {
-//   buf[0] = '\0';
-//   switch (choose(3)) {
-//     case 0: gen_num(); break;
-//     case 1: gen('('); gen_rand_expr(); gen(')'); break;
-//     default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
-//   }
-// }
 
-int main(){
-  gen_num();
-  gen_num();
-  gen_num();
-  gen_num();
-  gen_num();
-  gen_num();
+static void gen(char c){
+  buf[buf_idx] = c;
+  buf_idx++;
 }
-// int main(int argc, char *argv[]) {
-//   int seed = time(0);
-//   srand(seed);
-//   int loop = 1;
-//   if (argc > 1) {
-//     sscanf(argv[1], "%d", &loop);
-//   }
-//   int i;
-//   for (i = 0; i < loop; i ++) {
-//     gen_rand_expr();
-//
-//     sprintf(code_buf, code_format, buf);
-//
-//     FILE *fp = fopen("/tmp/.code.c", "w");
-//     assert(fp != NULL);
-//     fputs(code_buf, fp);
-//     fclose(fp);
-//
-//     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-//     if (ret != 0) continue;
-//
-//     fp = popen("/tmp/.expr", "r");
-//     assert(fp != NULL);
-//
-//     int result;
-//     ret = fscanf(fp, "%d", &result);
-//     pclose(fp);
-//
-//     printf("%u %s\n", result, buf);
-//   }
-//   return 0;
-// }
+
+static void gen_rand_op(){
+  char op;
+  switch (choose(4)) {
+    case 0: op = '+'; break;
+    case 1: op = '-'; break;
+    case 2: op = '*'; break;
+    default: op = '/'; break;
+  }
+  gen(op);  
+}
+
+static void gen_rand_expr() {
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
+}
+
+
+int main(int argc, char *argv[]) {
+  int seed = time(0);
+  srand(seed);
+  int loop = 1;
+  if (argc > 1) {
+    sscanf(argv[1], "%d", &loop);
+  }
+  int i;
+  for (i = 0; i < loop; i ++) {
+    gen_rand_expr();
+
+    sprintf(code_buf, code_format, buf);
+
+    FILE *fp = fopen("/tmp/.code.c", "w");
+    assert(fp != NULL);
+    fputs(code_buf, fp);
+    fclose(fp);
+
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    if (ret != 0) continue;
+
+    fp = popen("/tmp/.expr", "r");
+    assert(fp != NULL);
+
+    int result;
+    ret = fscanf(fp, "%d", &result);
+    pclose(fp);
+
+    printf("%u %s\n", result, buf);
+  }
+  return 0;
+}
