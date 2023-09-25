081a0045eadeff94681ad5ee061814e889e7d71f
ysyx_23060061-Guan Renyang <renyangguan@outlook.com>
2023-09-25 23:07:20 +0800

    Add a plain version of 'addi' without testing.

--- npc/vsrc/ysyx_23060061_Decoder.v
+++ npc/vsrc/ysyx_23060061_Decoder.v
@@ -1,7 +1,9 @@
 module ysyx_23060061_Decoder (
   input [6:0] opcode,
-  input [2:0] funct3
+  input [2:0] funct3,
+  output RegWrite
 );
-  
-
+  ysyx_23060061_MuxKeyWithDefault #(1, 10, 1) decoder(.out({RegWrite}), .key({opcode, funct3}), .default_out({1'b0}), .lut({
+    {7'b0010011,3'b000}, {1'b1}
+  }));
 endmodule
--- npc/vsrc/ysyx_23060061_Top.v
+++ npc/vsrc/ysyx_23060061_Top.v
@@ -4,14 +4,44 @@ module ysyx_23060061_Top (
   input clk,
   input rst, 
   input [ysys_23060061_Width-1 : 0] inst,
-  output [ysys_23060061_Width-1 : 0] snpc
+  output [ysys_23060061_Width-1 : 0] pc
 );
   // IF: reg PC and its updating rule.
   wire pc;
-  ysyx_23060061_Reg #(32, 32'h80000000) pc_reg(.clk(clk), .rst(rst), snpc, pc);
+  ysyx_23060061_Reg #(32, 32'h80000000) pc_reg(.clk(clk), .rst(rst), .din(snpc), .dout(pc), .wen(1'b1));
   assign snpc = pc + 4;
   
   // ID: Decoder unit
-   
+  wire RegWrite;
+  ysyx_23060061_Decoder(.opcode(inst[6:0]), .funct3(inst[14:12]), .RegWrite(RegWrite));
+  
+  wire [4:0] rs1;
+  wire [4:0] rs2;
+  wire [4:0] rd;
+  assign rs1 = inst[19:15];
+  assign rs2 = inst[24:20];
+  assign rd = inst[11:7];
+  
+  wire regData1;
+  wire regData2;
+  // Register File
+  ysys_23060061_RegisterFile #(5, 32) registerFile(
+    .clk(clk),
+    .wdata(aluOut),
+    .waddr(rd),
+    .wen(RegWrite),
+    .raddr1(rs1),
+    .raddr2(rs2),
+    .rdata1(regData1),
+    .rdata2(regData2)
+  );
+
+  // EX
+  wire [ysys_23060061_Width-1:0] imm;
+  assign imm = {20{inst[31]}, inst[31:20]}; // Sign extension
+
+  wire aluOut;
+  ysys_23060061_ALU(.clk(clk), .a(regData1), .b(imm), .aluOut(aluOut));
+
 endmodule
 
