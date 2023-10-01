4062556ffe31c3ddc8b479eddf545648a9928077
tracer-ysyx <tracer@ysyx.org>
2023-10-01 18:57:38 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  18:57:38 up  4:09,  2 users,  load average: 0.35, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -45,15 +45,15 @@ int main() {
   
   // cycle 2
   top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
-  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100000000000010011011; step_and_dump_wave();
+  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100000000000010010011; step_and_dump_wave();
   
   //cycle 3 
   top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
-  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010011011; step_and_dump_wave();
+  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010010011; step_and_dump_wave();
  
   //cycle 4 
   top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
-  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010011011; step_and_dump_wave();
+  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010010011; step_and_dump_wave();
 
   sim_exit();
 
