fa3b9e18ce07cbd3defaf306b2c19c57f35fea26
ysyx_23060061-Guan Renyang <renyangguan@outlook.com>
2023-10-02 21:16:37 +0800

    1. reset pc to 0x80000000 instead of 0x00000000 2. implement ebreak to trap using DPI-C machenism

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,10 +1,33 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-
+#include <inttypes.h>
+#include "svdpi.h"
+#include <stdlib.h>
+#include "Vysyx_23060061_Top__Dpi.h"
 #include <Vysyx_23060061_Top.h>
 
 #define TOPNAME Vysyx_23060061_Top
+#define MEMBASE 0x80000000
 
+// Memory
+static uint32_t instMem[] = {
+  0b00000000000100000000000010010011,
+  0b00000000000100001000000010010011,
+  0b00000000000100001000000010010011,
+  0b00000000000100001000000010010011,
+  0b00000000000000000000000001110011
+};
+
+
+static uint32_t paddr_read(uint32_t paddr){
+  printf("%u\n", paddr);
+  return instMem[(paddr-MEMBASE)/4];
+}
+
+bool Trap = false;
+void trap() {
+  Trap = true;
+}
 VerilatedVcdC* tfp = nullptr;
 VerilatedContext* contextp = NULL;
 static TOPNAME* top;
@@ -36,24 +59,29 @@ int main() {
   sim_init();
   
   // cycle 1 
-  top->clk = 0b1; top->rst = 0b1; step_and_dump_wave();
   top->clk = 0b0; top->rst = 0b1; step_and_dump_wave();
+  top->clk = 0b1; top->rst = 0b1; step_and_dump_wave();
+  top->clk = 0b0; top->rst = 0b0; step_and_dump_wave();
 
   // // cycle 2
   // top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
   // top->clk = 0b0; top->rst = 0b0; step_and_dump_wave();
   
   // cycle 2
-  top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
-  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100000000000010010011; step_and_dump_wave();
-  
-  //cycle 3 
-  top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
-  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010010011; step_and_dump_wave();
- 
-  //cycle 4 
-  top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
-  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010010011; step_and_dump_wave();
+  while(!Trap){
+    top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
+    top->clk = 0b0; top->rst = 0b0; top->inst = paddr_read(top->pc); step_and_dump_wave();
+  }
+ //  top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
+ //  top->clk = 0b0; top->rst = 0b0; top->inst = paddr_read(top->pc); step_and_dump_wave();
+ //  
+ //  //cycle 3 
+ //  top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
+ //  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010010011; step_and_dump_wave();
+ // 
+ //  //cycle 4 
+ //  top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
+ //  top->clk = 0b0; top->rst = 0b0; top->inst = 0b00000000000100001000000010010011; step_and_dump_wave();
 
   sim_exit();
 
