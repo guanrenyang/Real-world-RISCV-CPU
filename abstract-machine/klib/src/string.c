efe2b4cac5f3fdd792fa90a31966e8fcdf4ea347
ysyx_23060061-Guan Renyang <renyangguan@outlook.com>
2023-10-07 21:48:31 +0800

    pass test 'string'

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,31 +5,72 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t sum;
+
+  for (sum = 0; s[sum]!='\0'; sum++) {}
+
+  return sum;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    dst[i] = src[i];
+  dst[i] = '\0';
+
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for ( ; i < n; i++)
+    dst[i] = '\0';
+  
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dst_len = strlen(dst);
+  size_t i;
+
+  for (i = 0 ; src[i] != '\0' ; i++)
+      dst[dst_len + i] = src[i];
+  dst[dst_len + i] = '\0';
+
+  return dst;
+
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;  
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (i < n && s1[i] && s1[i] == s2[i]) {
+    i++;
+  }
+  if (i < n) {
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char* p = s;
+  while (n--) {
+    *p++ = (unsigned char)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +82,16 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = s1;
+  const unsigned char *p2 = s2;
+  
+  size_t i;
+  for (i = 0; i < n; ++i) {
+    if (p1[i] != p2[i]) {
+      return p1[i] - p2[i];
+    }
+  }
+  return 0;
 }
 
 #endif
