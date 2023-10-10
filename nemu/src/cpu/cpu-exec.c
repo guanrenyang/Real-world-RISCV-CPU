fce615865890df3df0b56c74394acb1b307fbd7b
tracer-ysyx <tracer@ysyx.org>
2023-10-10 15:44:57 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:18:46 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:44:57 up  5:27,  1 user,  load average: 0.70, 0.62, 0.52

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -59,33 +59,26 @@ static void exec_once(Decode *s, vaddr_t pc) {
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
-  // char *ring_p = s->iringbuf[g_nr_guest_inst % CONFIG_IRINGBUF_SIZE];
 
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  // ring_p += snprintf(ring_p, sizeof(s->iringbuf[g_nr_guest_inst % CONFIG_IRINGBUF_SIZE]), FMT_WORD ":", s->pc);
 
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
-    // ring_p += snprintf(ring_p, 4, " %02x", inst[i]);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
-  // memset(ring_p, ' ', space_len);
   p += space_len;
-  // ring_p += space_len;
 
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
-  // disassemble(ring_p, s->iringbuf[g_nr_guest_inst % CONFIG_IRINGBUF_SIZE] + sizeof(s->iringbuf[g_nr_guest_inst % CONFIG_IRINGBUF_SIZE]) - ring_p,
-      // MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
@@ -94,7 +87,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
 static void display_iringbuf(Decode *_this){
   Log("%s", "Recently executed instructions:");
-
+  Log("Instruction causing ABORT: %s", _this->iringbuf[g_nr_guest_inst % CONFIG_IRINGBUF_SIZE]);
+ 
   size_t i = 0;
   for( i = 0 ;i < CONFIG_IRINGBUF_SIZE && (int)(g_nr_guest_inst - 1 - i) >= 0; i++){
     Log("%s", _this->iringbuf[(g_nr_guest_inst - 1 - i) % CONFIG_IRINGBUF_SIZE]);
