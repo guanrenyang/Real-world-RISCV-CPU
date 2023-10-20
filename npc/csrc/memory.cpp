59b87013615f82ff6433b93438296563bd27b2d6
tracer-ysyx <tracer@ysyx.org>
2023-10-20 13:02:30 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  13:02:30 up  4:08,  0 users,  load average: 0.19, 0.15, 0.11

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -17,7 +17,7 @@ uint8_t* guest_to_host(uint32_t paddr) { return  instMem + paddr - MEMBASE; }
 //   0b00000000000100001000000010010011,
 //   0b00000000000000000000000001110011
 // }; void init_mem() {
-static inline uint32_t host_read(void *addr, int len){
+static uint32_t host_read(void *addr, int len){
   switch (len) {
     case 1: return *(uint8_t  *)addr;
     case 2: return *(uint16_t *)addr;
@@ -25,7 +25,7 @@ static inline uint32_t host_read(void *addr, int len){
   }
 }
 uint32_t paddr_read(uint32_t paddr){
-  printf("%u\n", paddr);
+  printf("%x\n", paddr);
   uint32_t ret = host_read(guest_to_host(paddr), 4);
   return ret;
 }
