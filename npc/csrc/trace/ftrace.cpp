e698f6e3355ab86b486d5b65d0a16b13ae596f22
tracer-ysyx <tracer@ysyx.org>
2023-11-08 13:08:29 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  13:08:29 up 10:51,  1 user,  load average: 0.45, 0.36, 0.32

--- npc/csrc/trace/ftrace.cpp
+++ npc/csrc/trace/ftrace.cpp
@@ -121,7 +121,7 @@ void source_func_addr(uint32_t addr, uint32_t* func_addr) {
     for (i = 0; i < symtab_size / sizeof(Elf32_Sym); i++) {
         if (ELF32_ST_TYPE(sym[i].st_info) == STT_FUNC) {
 			// printf("%d\n", i);
-			printf("%u %u\n", sym[i].st_value, sym[i].st_value + sym[i].st_size);
+			printf("%x %x\n", sym[i].st_value, sym[i].st_value + sym[i].st_size);
             if (addr >= sym[i].st_value && addr < sym[i].st_value + sym[i].st_size) {
                 // return the function name
                 // Log("function name: %s", strtab + sym[i].st_name);
