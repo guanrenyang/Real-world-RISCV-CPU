74ca76ece3c36e26e84f56c5be7a6321172e2f98
tracer-ysyx <tracer@ysyx.org>
2023-11-20 01:18:21 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  01:18:21 up  4:10,  0 users,  load average: 0.37, 0.24, 0.19

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -75,9 +75,11 @@ void sim_exit() {
 static int inst_cnt = 0;
 void exec_once() {
 	
-	printf("MemRW before the next clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__MemRW);
-	printf("memAddr before the next clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOut);
-
+	// printf("MemRW before the next clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__MemRW);
+	// printf("memAddr before the next clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOut);
+	printf("aluOpA before the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOpA);
+	printf("aluOpB before the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOpB);
+	printf("aluOp before the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOp);
 	top->clk = 0b1; top->rst = 0b0; step_and_dump_wave();
 
 	printf("pc = %x\n", top->pc);
@@ -94,9 +96,11 @@ void exec_once() {
 #endif 
 	// printf("dnpc = %x\n", top->ftrace_dnpc); // Here, dnpc equals to pc+4
 	step_and_dump_wave();
-	printf("MemRW after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__MemRW);
-	printf("memAddr after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOut);
-
+	// printf("MemRW after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__MemRW);
+	// printf("memAddr after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOut);
+	printf("aluOpA after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOpA);
+	printf("aluOpB after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOpB);
+	printf("aluOp after the current clock: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOp);
 	// printf("dnpc after = %x\n", top->ftrace_dnpc); // Here, dnpc is the right dnpc
 
 #ifdef CONFIG_FTRACE
