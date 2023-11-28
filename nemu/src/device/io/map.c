ec92e3eead4f166388082564b1e46af2845225d0
tracer-ysyx <tracer@ysyx.org>
2023-11-28 11:21:22 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  11:21:22 up  1:08,  1 user,  load average: 0.47, 0.69, 0.72

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -54,6 +54,7 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  printf("map_read: addr = " FMT_PADDR ", len = %d\n", addr, len);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
