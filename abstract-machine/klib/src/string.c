4b40db8b467545d3ef4ccedabdec141cb6847f2f
tracer-ysyx <tracer@ysyx.org>
2023-11-28 10:47:14 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  10:47:14 up 34 min,  0 users,  load average: 0.28, 0.33, 0.27

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -74,12 +74,13 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  char * tmp = (char *)malloc(n);
+  // char * tmp = (char *)malloc(n);
 
-  memcpy(tmp, src, n);
-  memcpy(dst, tmp, n);
+  // memcpy(tmp, src, n);
+  // memcpy(dst, tmp, n);
 
-  free(tmp);
+  memcpy(dst, src, n);
+  // free(tmp);
   
   return dst;
 }
