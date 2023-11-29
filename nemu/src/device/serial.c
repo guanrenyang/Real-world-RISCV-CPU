296d0adf5e465a2acc3d69724dee9bc23d71fa99
tracer-ysyx <tracer@ysyx.org>
2023-11-29 15:54:23 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:54:23 up 44 min,  0 users,  load average: 0.37, 0.38, 0.31

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -25,7 +25,7 @@ static uint8_t *serial_base = NULL;
 
 
 static void serial_putc(char ch) {
-	fprintf(stderr, "serial_putc: %x;\n", ch);
+	fprintf(stderr, "\nserial_putc: %x;\n", ch);
   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
 }
 
