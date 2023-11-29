f8e6f0e0f167808d8d57297eab3275c7259eb552
tracer-ysyx <tracer@ysyx.org>
2023-11-29 15:41:07 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:41:07 up 31 min,  0 users,  load average: 0.19, 0.24, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -58,6 +58,11 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
       switch (fmt[++i]) {
 	      case 'd':
 	        d = va_arg(ap, int);
+			putch((char)d);
+			putch((char)(d>>8));
+			putch((char)(d>>16));
+			putch((char)(d>>24));
+
 	        char str_d[20];
 	        itoa(d, str_d);
 	        strcpy(out+out_i, str_d);
