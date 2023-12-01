46631db22ffaf504040b252158dcda9fbe45528c
tracer-ysyx <tracer@ysyx.org>
2023-12-01 10:33:51 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  10:33:51 up  1:30,  0 users,  load average: 0.26, 0.21, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -43,12 +43,16 @@ static char* itoa(int n, char* str) {
   char swap_tmp;
   size_t num_bit = i;
   swap_tmp = str[0];
-  str[0] = str[3];
-  str[3] = swap_tmp;
   for (size_t i = 0; i < 4; i++)
   {
 	putch(str[i]); putch(' ');
   }
+  str[0] = str[3];
+  str[3] = swap_tmp;
+ //  for (size_t i = 0; i < 4; i++)
+ //  {
+	// putch(str[i]); putch(' ');
+ //  }
   
   for ( i = 0; i < num_bit / 2; i++) {
 	// putch(str[i]); putch(str[num_bit-1-i]); putch('\n');
