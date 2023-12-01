de4244be069b62dd71e80b7da17004d537b5b02d
tracer-ysyx <tracer@ysyx.org>
2023-12-01 10:25:58 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  10:25:58 up  1:22,  0 users,  load average: 0.23, 0.27, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -35,9 +35,9 @@ static char* itoa(int n, char* str) {
 	// putch(' ');
  //  }
 
-  for (size_t i = 0; i < 4; i++) {
-	  putch(str[i]);putch('\n');
-  }
+  // for (size_t i = 0; i < 4; i++) {
+	 //  putch(str[i]);putch('\n');
+  // }
    
   // in-place reversion
   char swap_tmp;
