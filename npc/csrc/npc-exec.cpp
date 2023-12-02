81e1abfefe9995c72f2943c267016e3a3b022120
ysyx_23060061-Guan Renyang <renyangguan@outlook.com>
2023-12-02 20:25:52 +0800

    1. fix bug: pmem_write always writes 4 bytes although the wdata is    masked by wmask, which is wrong for lb and lh. 2. complete serial port and timer for npc

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -91,7 +91,7 @@ void exec_once() {
 	}
 #endif
 
-	top->clk = 0b0; top->rst = 0b0; top->inst = paddr_read(top->pc, 4); 
+	top->clk = 0b0; top->rst = 0b0; top->inst = pmem_read(top->pc, 4); 
 
 #ifdef CONFIG_ITRACE
 	itrace(top->pc, top->inst, 4);
