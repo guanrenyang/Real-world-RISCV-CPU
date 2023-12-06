705800fa2b388f9ab5f57c06e093a1914adb9d14
tracer-ysyx <tracer@ysyx.org>
2023-12-07 00:49:20 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  00:49:20 up 43 min,  1 user,  load average: 0.66, 0.35, 0.20

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -19,8 +19,12 @@
 #include <common.h>
 
 typedef struct {
+  /* General purpose registers*/
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
+  
+  /* */
+
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
