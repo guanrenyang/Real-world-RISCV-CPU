e564b1310f66df404c584252a3995ccc99456aa1
tracer-ysyx <tracer@ysyx.org>
2023-12-07 14:02:27 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  14:02:27 up  2:22,  0 users,  load average: 0.33, 0.38, 0.36

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -103,7 +103,7 @@ void source_func_name(vaddr_t addr, char* func_name){
         if (ELF32_ST_TYPE(sym[i].st_info) == STT_FUNC) {
             if (addr >= sym[i].st_value && addr < sym[i].st_value + sym[i].st_size) {
                 // return the function name
-                // Log("function name: %s", strtab + sym[i].st_name);
+                Log("function name: %s", strtab + sym[i].st_name);
                 strcpy(func_name, strtab + sym[i].st_name);
                 return;
             }
