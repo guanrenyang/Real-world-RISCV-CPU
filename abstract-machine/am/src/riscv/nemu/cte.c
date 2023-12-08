feedd58df9ea6daca9adf29880dd2e2d54fdf056
tracer-ysyx <tracer@ysyx.org>
2023-12-08 15:12:28 +0800

    > compile NEMU ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:12:28 up 15:17,  0 users,  load average: 0.34, 0.17, 0.11

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -34,6 +34,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 #define CONTEXT_SIZE (32+3+1)*32/8
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   // Context *ctx = (Context *)((uint8_t *)kstack.end - CONTEXT_SIZE);
+  // ctx->
   return NULL;
 }
 
