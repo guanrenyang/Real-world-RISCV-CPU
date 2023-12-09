d7d052948fe4c8d07022a51c98fbe24f2f8c4647
tracer-ysyx <tracer@ysyx.org>
2023-12-09 23:31:17 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-86-generic #96-Ubuntu SMP Wed Sep 20 08:29:36 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  23:31:17 up 14 min,  0 users,  load average: 0.40, 0.45, 0.32

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -112,6 +112,7 @@ void exec_once() {
 		
 		printf("aluOut: %x\n", top->rootp->ysyx_23060061_Top__DOT__aluOut);
 		printf("src1: %x\n", top->rootp->ysyx_23060061_Top__DOT__regData1);
+		printf("src2: %x\n", top->rootp->ysyx_23060061_Top__DOT__regData2);
 		top->clk = 0b0; top->rst = 0b0; top->inst = pmem_read(top->pc, 4);  step_and_dump_wave();
 		printf("mcause: %x\n", top->rootp->ysyx_23060061_Top__DOT__CSRs__DOT__rf[3]);
 		printf("pc: %x\n", top->pc);
