9ca2c4cb91dbe7ce209eafac37ce526fc9dedcf2
tracer-ysyx <tracer@ysyx.org>
2024-01-17 10:52:57 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  10:52:57 up 47 min,  1 user,  load average: 0.11, 0.18, 0.18

--- npc/vsrc/ysyx_23060061_IFU.v
+++ npc/vsrc/ysyx_23060061_IFU.v
@@ -8,22 +8,22 @@ module ysyx_23060061_IFU_with_SRAM(
 	output reg instValid,
 	input iduReady,
 	
-	output reg [31:0] araddr,
-	output reg arvalid,
+	output [31:0] araddr,
+	output arvalid,
 	input arready,
 
 	input [31:0] rdata,
 	input [1:0] rresp,
 	input rvalid,
-	output reg rready,
+	output rready,
 
 	output [31:0] awaddr,
-	output reg awvalid,
+	output awvalid,
 	input awready,
 
 	output [31:0] wdata,
 	output [3:0] wstrb,
-	output reg wvalid,
+	output wvalid,
 	input wready,
 
 	input bresp,
@@ -35,63 +35,24 @@ module ysyx_23060061_IFU_with_SRAM(
 
 	reg [31:0] pc_old;
 	
-	// // Combinationa logic
-	// always @(pc!=pc_old) begin
-	// 	if (!rst) begin
-	// 		paddr_read(pc, instImm_internal);
-	// 	end
-	// end
-
-	parameter IDLE = 0;
-	parameter SEND_ADDR = 1;
-	parameter WAIT_DATA = 2; 
-	
-	reg [1:0] state;
+	// Combinationa logic
+	always @(pc!=pc_old) begin
+		if (!rst) begin
+			paddr_read(pc, instImm_internal);
+		end
+	end
 
 	// Sequential logic
 	always @(posedge clk) begin
-		if (~rst) begin // do reset
-			// signals for InstMem
-			arvalid <= 0;	
-			awvalid <= 0;
-			wvalid <=0;
-			// signals for IDU				
+		if (rst) begin
 			inst <= 0;
 			pc_old <= 0;
 			instValid <= 0;
 		end else begin
-			case (state)
-				IDLE: begin
-					if(pc_old != pc) begin// need to read InstMem
-						state <= SEND_ADDR; // state transition
-						
-						araddr <= pc;
-						arvalid <= 1;
-						rready <= 0;
-					end
-				end
-				SEND_ADDR: begin
-					if (arvalid && arready) begin
-						state <= WAIT_DATA;
-						arvalid <= 0;
-						rready <= 1;		
-					end
-				end
-				WAIT_DATA: begin
-					if (rvalid & rready) begin
-						state <= IDLE;
-						rready <= 0;
-						inst <= rdata;
-						instValid <= 1;
-					end
-				end
-			endcase
+			inst <= instImm_internal;
 			pc_old <= pc;
-
-			// inst <= instImm_internal;
-			// pc_old <= pc;
-			// // instValid <= (pc_old == pc) ? 0 : 1;
-			// instValid <= ~iduReady;
+			// instValid <= (pc_old == pc) ? 0 : 1;
+			instValid <= ~iduReady;
 		end
 	end	
 endmodule
