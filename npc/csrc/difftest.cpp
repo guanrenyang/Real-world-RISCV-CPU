140c9b554196922a26bf811f2b9ea21d1adf08c8
tracer-ysyx <tracer@ysyx.org>
2024-01-18 22:38:54 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  22:38:54 up 19:44,  1 user,  load average: 0.26, 0.27, 0.39

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -90,7 +90,7 @@ void difftest_step(uint32_t pc, uint32_t npc, bool enableMemCheck = false) {
 		uint8_t *ref_mem = (uint8_t*) malloc(MEMSIZE);
 		ref_difftest_memcpy(MEMBASE, ref_mem, MEMSIZE, DIFFTEST_TO_DUT);
 		// printf("%x\n",  [0x80008ffc-MEMBASE]);
-		printf("80000262: %x\n", *(uint32_t *)(ref_mem+0x80000262-MEMBASE));
+		printf("80008fe0: %x\n", *(uint32_t *)(ref_mem+0x80008fe0-MEMBASE));
 		// if(!checkmem(ref_mem, MEMBASE, MEMSIZE)){
 		// 	sim_exit();
 		// 	fprintf(stderr, "difftest failed with executed pc=%x, cpu_npc=%x, ref_state->pc=%x\n", pc, npc, ref_state.pc);
