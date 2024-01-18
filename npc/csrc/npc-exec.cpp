7173a0cfe0f133564a3fb14a0a5367f7ae7955c0
tracer-ysyx <tracer@ysyx.org>
2024-01-18 23:36:47 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  23:36:47 up 20:42,  1 user,  load average: 0.66, 0.46, 0.65

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -125,7 +125,8 @@ void exec_once() {
 	top->clk = 0b0; top->rst = 0b0; // top->inst = pmem_read(top->pc, 4); 
 
 #ifdef CONFIG_ITRACE
-	itrace(PC_, pmem_read(PC_, 4), 4);
+	if(PC_!=pc_old)
+		itrace(PC_, pmem_read(PC_, 4), 4);
 #endif 
 	// printf("dnpc = %x\n", top->ftrace_dnpc); // Here, dnpc equals to pc+4
 	step_and_dump_wave();
--- npc/csrc/trace/itrace.cpp
+++ npc/csrc/trace/itrace.cpp
@@ -6,7 +6,6 @@ char logbuf[128];
 char iringbuf[128][128];
 
 void itrace(uint32_t pc, uint32_t ival, int ilen /*bytes*/){
-	static int tmp = 0;
     char *p = logbuf;
     p += snprintf(p, sizeof(logbuf), FMT_WORD ":", pc);
 
@@ -22,9 +21,6 @@ void itrace(uint32_t pc, uint32_t ival, int ilen /*bytes*/){
   disassemble(p, logbuf + sizeof(logbuf) - p, pc, (uint8_t *)&ival, ilen);
 
   log_write("%s\n", logbuf);
-  if(tmp==3)
-  	exit(0);
-  tmp++;
 }
 
 #else
