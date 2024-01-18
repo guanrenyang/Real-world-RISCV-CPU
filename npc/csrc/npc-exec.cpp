cf390ebd8eb56c7b7941aaa99008d7baeaf8a7b8
tracer-ysyx <tracer@ysyx.org>
2024-01-18 22:28:26 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  22:28:26 up 19:33,  1 user,  load average: 0.25, 0.47, 0.56

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -116,8 +116,8 @@ void exec_once() {
 #ifdef CONFIG_DIFFTEST
 	if (PC_ != pc_old){ 
 		uint32_t inst_old = pmem_read(pc_old, 4);
-		// difftest_step(PC_, DNPC_, (inst_old & 0x0000007f) == 0x23); 
-		difftest_step(PC_, DNPC_, false);
+		difftest_step(PC_, DNPC_, (inst_old & 0x0000007f) == 0x23); 
+		// difftest_step(PC_, DNPC_, false);
 		pc_old = PC_;
 	}
 #endif
