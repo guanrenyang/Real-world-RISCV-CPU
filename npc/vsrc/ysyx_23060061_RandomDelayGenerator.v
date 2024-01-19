17e76c99585451e0accd4eaf0833fa19438ca844
tracer-ysyx <tracer@ysyx.org>
2024-01-19 12:53:19 +0800

    > sim RTL ysyx_23060061 管仁阳 Linux ubuntu-linux-22-04-desktop 5.15.0-91-generic #101-Ubuntu SMP Tue Nov 14 13:29:11 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  12:53:19 up 1 day,  1:50,  1 user,  load average: 0.80, 0.45, 0.45

--- npc/vsrc/ysyx_23060061_RandomDelayGenerator.v
+++ npc/vsrc/ysyx_23060061_RandomDelayGenerator.v
@@ -3,16 +3,23 @@ module ysyx_23060061_RandomDelayGenerator (
 	input rst,
 	output delay_trigger
 );
-	localparam [31:0] DELAY = 30;
+
+	wire [31:0] random_number;
+	ysyx_23060061_lfsr_random_generator #(32) lfsr_random_generator (
+		.clk(clk),
+		.reset(rst),
+		.random_number(random_number)
+	);
+
 	reg [31:0] delay;
 	reg [31:0] delay_counter;
 	always @(posedge clk) begin
 		if (~rst) begin
-			delay <= $random % 1000;
+			delay <= random_number;
 			delay_counter <= 0;
 		end else begin
 			if (delay_counter == delay) begin
-				delay <= $random % 1000;
+				delay <= random_number;
 				delay_counter <= 0;
 			end else begin
 				delay <= delay;
@@ -22,4 +29,27 @@ module ysyx_23060061_RandomDelayGenerator (
 	end
 
 	assign delay_trigger = (delay_counter == delay);
+endmodule
+
+module ysyx_23060061_lfsr_random_generator #(NR_BIT=32)(
+    input clk,
+    input reset, // active low
+    output [NR_BIT-1:0] random_number
+);
+
+reg [NR_BIT-1:0] lfsr_reg;
+
+// 定义反馈多项式
+wire feedback = lfsr_reg[NR_BIT-1] ^ lfsr_reg[NR_BIT-2]; // 示例：x^8 + x^7 + 1
+
+assign random_number = lfsr_reg;
+
+always @(posedge clk ) begin
+    if (~reset) begin
+        lfsr_reg <= {NR_BIT{1'b1}}; // 非全零初始值
+    end else begin
+        lfsr_reg <= {lfsr_reg[NR_BIT-2:0], feedback};
+    end
+end
+
 endmodule
